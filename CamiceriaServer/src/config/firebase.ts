f92f9a617212ebdbeae0856bb27d8ce979faad2f
stefanopiccoli <stefano.piccoli2800@gmail.com>
2023-05-03 23:30:24 +0200

    .env SDK firbease

--- CamiceriaServer/src/config/firebase.ts
+++ CamiceriaServer/src/config/firebase.ts
@@ -1,26 +1,25 @@
 import admin from "firebase-admin";
-import serviceAccount from '../SDKAdminKey.json' assert {type: "json"};
+// import serviceAccount from '../../' assert {type: "json"};
 import dotenv from "dotenv";
 
 
 dotenv.config();
 
-// const conf = {
-//   type: serviceAccount.type,
-//   project_id: serviceAccount.project_id,
-//   private_key_id: serviceAccount.private_key_id,
-//   private_key: serviceAccount.private_key,
-//   client_email: serviceAccount.client_email,
-//   client_id: serviceAccount.client_id,
-//   auth_uri: serviceAccount.auth_uri,
-//   token_uri: serviceAccount.token_uri,
-//   auth_provider_x509_cert_url: serviceAccount.auth_provider_x509_cert_url,
-//   client_x509_cert_url: serviceAccount.client_x509_cert_url,
-// };
-
+const cred = {
+  type : process.env.TYPE,
+  project_id : process.env.PROJECT_ID,
+  private_key_id : process.env.PRIVATE_KEY_ID,
+  private_key : process.env.PRIVATE_KEY,
+  client_email : process.env.CLIENT_EMAIL,
+  client_id : process.env.CLIENT_ID,
+  auth_uri : process.env.AUTH_URI,
+  token_uri : process.env.TOKEN_URI,
+  auth_provider_x509_cert_url : process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url : process.env.CLIENT_X509_CERT_URL,
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any)
+  credential: admin.credential.cert(JSON.parse(JSON.stringify(cred)))
 });
 
 
